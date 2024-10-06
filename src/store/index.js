3bc92f65380d22f1fb7513fe5e8e595b0e18cd5b
Hossam Ayman <hhasanelbadry2000@gmail.com>
2024-10-06 15:30:12 +0300

    get started with vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from "vuex";
-
+//centralized store
 export default createStore({
+  //data
   state: {},
   getters: {},
   mutations: {},
+  //methods
   actions: {},
   modules: {},
 });
